6706ad249124e9b307ccc495ebf292effc177deb
Tasfiajahan <tania.develpers@gmail.com>
2022-11-30 17:33:14 +0600

    my 15th commit

--- src/Pages/Home/Components/Products/Products.js
+++ src/Pages/Home/Components/Products/Products.js
@@ -25,7 +25,7 @@ const Products = () => {
                 !loader && <div div class="mb-12 md:mb-14 xl:mb-16 rounded-md pt-5 md:pt-6 lg:pt-7 pb-5 lg:pb-7 px-4" >
                     <div class="flex justify-center items-center flex-wrap mb-5 md:mb-6">
                         <div class="flex items-center justify-center -mt-2 mb-0">
-                            <h3 class="text-center  font-bold text-heading">All Product</h3>
+                            <h3 class="text-center  font-bold text-heading text-3xl">All Product</h3>
                         </div>
                     </div>
                     <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-x-3 md:gap-x-5 xl:gap-x-7 gap-y-4 lg:gap-y-5 xl:gap-y-6 2xl:gap-y-8">
